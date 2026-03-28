8f0660c519a45493cbaf30dc2aa77f3d73b3a977
Miguel Ramos <return.infox@gmail.com>
2026-03-28 13:00:47 -0300

    codigo actualizado con la solucion del ejercicio

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from crud_app.views import ProductoCreateView, ProductoListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ProductoListView.as_view(), name='listar'),
     path('nuevo/', ProductoCreateView.as_view(), name='crear'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
--- crud_app/views.py
+++ crud_app/views.py
@@ -3,13 +3,14 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 from .models import Producto
 from .forms import ProductoForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ProductoListView(ListView):
     model = Producto
     template_name = 'listar.html'
     context_object_name = 'productos'
 
-class ProductoCreateView(CreateView):
+class ProductoCreateView(LoginRequiredMixin,CreateView):
     model = Producto
     form_class = ProductoForm
     template_name = 'form.html'
